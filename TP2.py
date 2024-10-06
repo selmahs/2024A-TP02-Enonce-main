a43212dd48a0e712794fbb67b280b2be49c2aa50
selmahs <selmahseine@gmail.com>
2024-10-05 21:16:47 -0400

    CHANGEMENT

--- TP2.py
+++ TP2.py
@@ -18,9 +18,9 @@ c= csv.DictReader(csvfile)
 for row in c:
     cote_rangement = row["cote_rangement"]
     bibliotheque[cote_rangement] = {
-        "Titre:" : row["titre"],
-        "Auteur:" : row["auteur"],
-        "date_de_publication:" : row["date_publication"],
+        "titre" : row["titre"],
+        "auteur" : row["auteur"],
+        "date_publication" : row["date_publication"],
     }
 
 #print(f' \n Bibliotheque initiale : {bibliotheque} \n')
@@ -38,9 +38,9 @@ for row in v:
     cote_rangement = row["cote_rangement"]
     if cote_rangement not in bibliotheque:
         bibliotheque[cote_rangement] = {
-            "Titre:" : row['titre'],
-            "Auteur:" : row['auteur'],
-            "date_de_publication:" : row['date_publication'],
+            "titre" : row['titre'],
+            "auteur" : row['auteur'],
+            "date_publication" : row['date_publication'],
         }
         print (f"Le livre {cote_rangement} ---- {row['titre']} par {row['auteur']} ---- a été ajouté avec succès")
     else:
@@ -54,18 +54,15 @@ for row in v:
 # TODO : Écrire votre code ici
 modif = {}
 for coteRangement, details in bibliotheque.items():
+    # if "auteur" in details.keys() and details["auteur"] == "William Shakespeare" and coteRangement.startswith("S"):
+    if details["auteur"] == "William Shakespeare" and coteRangement.startswith("S"):
+        newCote = "WS" + coteRangement[1:]
+        modif[coteRangement] = newCote
 
-    for coteRangement, details in bibliotheque.items():
-        if "auteur" in details.keys() and details["auteur"] == "William Shakespeare" and coteRangement.startswith("S"):
-            newCote = "WS" + coteRangement[1:]
-            modif[coteRangement] = newCote
-
-    for ancienneCote, newCote in modif.items():
-        if ancienneCote in bibliotheque:
-            bibliotheque[ancienneCote] = bibliotheque.pop(coteRangement[0])
+for ancienneCote, newCote in modif.items():
+    bibliotheque[newCote] = bibliotheque.pop(ancienneCote)
 
 print(f'\nBibliothèque avec modifications de cote : {bibliotheque}\n')
-
 ########################################################################################################## 
 # PARTIE 4 : Emprunts et retours de livres
 ########################################################################################################## 
@@ -84,38 +81,24 @@ for row in c:
         bibliotheque[cote_rangement]['emprunts']= "emprunte"
         bibliotheque[cote_rangement]['date_emprunt']= row['date_emprunt']
        
-print(f"\n Bibliotheque avec ajout des emprunts : {bibliotheque} \n")
+#print(f"\n Bibliotheque avec ajout des emprunts : {bibliotheque} \n")
 
 
 ########################################################################################################## 
 # PARTIE 5 : Livres en retard 
 ########################################################################################################## 
-from datetime import datetime
+import datetime
 
-from datetime import timedelta
+dateActuelle = datetime.datetime.now()
 
-frais_retard_jour=2
-frais_maximal=100
-date_today=datetime.now()
-delai_de_retour=timedelta (days=30)
-livre_perdu=[]
+for cote_rangement, detail in bibliotheque.items():
+    if details["emprunts"] == "emprunte":
+        date_emprunt = datetime.datetime.strptime(detail["date_emprunt"], "%Y -%m-%d")
+        dateDiff = (dateActuelle-date_emprunt).days
+        if dateDiff>30:
+            frais= min((dateDiff-30)*2,100)
+            detail["frais_retard"] = frais
+        if dateDiff>365:
+            detail["livre_perdu"]= True
 
-for cote, informations in bibliotheque.items():
-  if informations["emprunté"]=="emprunt":
-    date_emprunt=datetime.strptime(informations["date_emprunt"], "%Y-%m-$d")
-    late_days=(date_today-date_emprunt).days-delai_de_retour.jours
-
-    if late_days>0:
-      frais_retard_jour=str(min(late_days*frais_retard_jour,frais_maximal))+"$"
-      bibliotheque[cote]["frais_retard"]=frais_retard_jour
-    if late_days>365:
-      livre_perdu.append(cote)
-      bibliotheque[cote]["livre_perdu"]="Oui"
-    if late_days<=365:
-      livre_perdu.append(cote)
-      bibliotheque[cote]["livre_perdu"]="Non"
-    else:
-      bibliotheque[cote]["livre_perdu"]="Non"
-
-
-print(f"\nBibliotheque avec ajout des retards et frais: {bibliotheque} \n")
+#print(f"\n Bibliotheque avec ajout des retards et frais : {bibliotheque} \n")
